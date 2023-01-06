98139aeeb06964b0e071ed511f3deda9d0a44c13
huiseung <whtjddn75@gmail.com>
2023-01-06 13:31:16 +0900

    first commit

---
+++ src/test/java/com/example/trafficbankbatch/TrafficBankBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trafficbankbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficBankBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
